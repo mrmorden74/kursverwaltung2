5dc4a2fde7d07b679d573e341f1a1842c3d2450f
mrmorden74 <a.hitzinger@gmail.com>
2016-11-10 12:22:59 +0100

    get und set für Kunde

--- Kursverwaltung/Personen/Kunde.class.php
+++ Kursverwaltung/Personen/Kunde.class.php
@@ -1,7 +1,42 @@
 <?php 
 namespace Kursverwaltung\Personen;
 class Kunde {
+
 	public function test () {
 		return 'test';
 	}
+
+	private $id;
+	private $kundennummer;
+	private $vorname;
+	private $nachname;
+	private $adresse;
+	private $plz;
+	private $ort;
+	private $telefon;
+	private $email;
+
+	public function __construct() {
+
+	}
+
+	public function __set($name, $value)
+	{
+		if (property_exists($this, $name)) {
+			$this->$name = $value;
+		} else {
+				throw new \Exception('Ungültiger Zugriff!!!!!!!!!!!!!!!');	
+		}
+	}
+
+	public function __get($name) {
+		if (property_exists($this, $name)) {
+			return $this->$name;
+		} else {
+				throw new \Exception('Ungültiger Zugriff!!!!!!!!!!!!!!!');	
+		}
+
+	}
+
+
 }
--- index.php
+++ index.php
@@ -9,8 +9,11 @@ require_once('inc/init.inc.php');
 </head>
 <body>
 	<?php
-	use Kursverwaltung\Kurse\Kurs;
-	$test = new Kurs();
+	use Kursverwaltung\Personen\Kunde;
+	// use Kursverwaltung\Kurse\Kurs;
+	$test = new Kunde();
+	$test->vorname = 'Habakuk'; // ruft __set auf
+	echo $test->vorname; // ruft __get auf
 	var_dump($test);
 	?>
 </body>
