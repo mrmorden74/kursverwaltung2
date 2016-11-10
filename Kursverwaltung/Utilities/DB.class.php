48250b98d26bb6cc047f6d178fcf5821f302770a
mrmorden74 <a.hitzinger@gmail.com>
2016-11-10 15:12:52 +0100

    connection

--- Kursverwaltung/Utilities/DB.class.php
+++ Kursverwaltung/Utilities/DB.class.php
@@ -1,31 +1,40 @@
 <?php
-namespace Kursverwaltung\Utilities; 
-class DB {
-	private static $connection;
+namespace Kursverwaltung\Utilities;
 
-	public static function insert(object $value) {
-		//check ob Connection vorhanden ist
-		self::checkConnection();
+class DB
+{
+    private static $connection;
 
-	}
-	
-	private static function checkConnection() {
-		if (self::$connection === null) {
-			self::$connection = self::getConnection;
-		}
-	}
+    public static function insert(object $value)
+    {
+        //check ob Connection vorhanden ist
+        self::checkConnection();
+    }
+    
+    private static function checkConnection()
+    {
+        if (self::$connection === null) {
+            self::$connection = self::getConnection;
+        }
+    }
 
-	/**
-	* Erstellt Datenbankverbindung
-	* @return resource mysqli Connection
-	*/
-	private static function getConnection() {
-		$config = Config::getDbConfig();
-		$conn = new \mysqli($config['host'], $config['user'], $config['passwort'], $config['database']);
+    /**
+    * Erstellt Datenbankverbindung
+    * @return resource mysqli Connection
+    */
+    private static function getConnection()
+    {
+        $config = Config::getDbConfig();
+        $conn = new \mysqli(
+            $config['host'],
+            $config['user'],
+            $config['passwort'],
+            $config['database']
+        );
 
-		if ($mysqli->connect_error !== null) {
-			throw new \Exception('Datenbank Verbindungsfehler');
-		}
-		return $conn; 
-	}
+        if ($mysqli->connect_error !== null) {
+            throw new \Exception('Datenbank Verbindungsfehler');
+        }
+        return $conn;
+    }
 }
